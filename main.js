4aa0aa97097d3825a4860b28d5a7364d092454e2
xiaozhi1 <qasxed5@163.com>
2018-03-31 22:26:00 +0800

    优化第二个版本

--- main.js
+++ main.js
@@ -1,21 +1,40 @@
-$('.images > img:nth-child(1)').addClass('current')
-$('.images > img:nth-child(2)').addClass('enter')
-$('.images > img:nth-child(3)').addClass('enter')
-let n = 1
+let n
+init()   //初始化
 setInterval(()=>{
-  $(`.images > img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
+  makeLeave(getImage(n))
     .one('transitionend', (e)=>{
-      $(e.currentTarget).removeClass('leave').addClass('enter')
+      makeEnter($(e.currentTarget))
     })
-  $(`.images > img:nth-child(${x(n+1)})`).removeClass('enter').addClass('current')
+  makeCurrent(getImage(n+1))
   n += 1
-},1500)
+},3000)
+
+
+
+// 下面各种函数封装，可以不看
+function getImage(n){
+  return $(`.images > img:nth-child(${x(n)})`)
+}
 function x(n){
   if(n>3){
     n = n%3
     if (n===0){
       n =3
     }
-  } 
+  } // n = 1 2 3
   return n
+}
+function init(){
+  n = 1
+  $(`.images > img:nth-child(${n})`).addClass('current')
+    .siblings().addClass('enter')
+}
+function makeCurrent($node){
+  return $node.removeClass('enter leave').addClass('current')
+}
+function makeLeave($node){
+  return $node.removeClass('enter current').addClass('leave')
+}
+function makeEnter($node){
+  return $node.removeClass('leave current').addClass('enter')
 }
