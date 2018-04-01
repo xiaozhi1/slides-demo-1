08212ea8cde9a5ff863feead7d2f1bdc4806ba08
xiaozhi1 <qasxed5@163.com>
2018-04-01 18:42:16 +0800

    无缝轮播

--- main.js
+++ main.js
@@ -1,55 +1,47 @@
-let n
-init()   //初始化
-let timer = setInterval(()=>{
-  makeLeave(getImage(n))
-    .one('transitionend', (e)=>{
-      makeEnter($(e.currentTarget))
-    })
-  makeCurrent(getImage(n+1))
-  n += 1
-},2000)
+let $buttons = $('#buttonWrapper > button')
+let $slides = $('#slides')
+let $images = $slides.children('img')
+let firstCopy = $images.eq(0).clone(true)
+let lastCopy = $images.eq($images.length-1).clone(true)
 
-document.addEventListener('visibilitychange',function(e){
-  // console.log(document.hidden)
-  if(document.hidden){
-    window.clearInterval(timer)
-  }else{
-    timer = setInterval(()=>{
-      makeLeave(getImage(n))
-        .one('transitionend', (e)=>{
-          makeEnter($(e.currentTarget))
-        })
-      makeCurrent(getImage(n+1))
-      n += 1
-    },2000)
+$slides.append(firstCopy)
+$slides.prepend(lastCopy)
+console.log($slides[0])
+$slides.css({transform:'translateX(-400px)'})
+
+let current = 0
+
+$buttons.eq(0).on('click',function(){
+  if(current == 2){
+    console.log('说明你是从最后一张到第一张')
+    $slides.css({transform:'translateX(-1600px)'})
+      .one('transitionend',function(){
+        $slides.hide()
+          .offset()
+        $slides.css({transform:'translateX(-400px)'})
+          .show()
+      })
+  } else{
+    $slides.css({transform:'translateX(-400px)'})
   }
+  current = 0
 })
-
-
-// 下面各种函数封装，可以不看
-function getImage(n){
-  return $(`.images > img:nth-child(${x(n)})`)
-}
-function x(n){
-  if(n>3){
-    n = n%3
-    if (n===0){
-      n =3
-    }
-  } // n = 1 2 3
-  return n
-}
-function init(){
-  n = 1
-  $(`.images > img:nth-child(${n})`).addClass('current')
-    .siblings().addClass('enter')
-}
-function makeCurrent($node){
-  return $node.removeClass('enter leave').addClass('current')
-}
-function makeLeave($node){
-  return $node.removeClass('enter current').addClass('leave')
-}
-function makeEnter($node){
-  return $node.removeClass('leave current').addClass('enter')
-}
+$buttons.eq(1).on('click',function(){
+  console.log(current)
+  $slides.css({transform:'translateX(-800px)'})
+})
+$buttons.eq(2).on('click',function(){
+  if(current == 0){
+    console.log('说明你是从第一张到最后一张')
+    $slides.css({transform:'translateX(0px)'})
+      .one('transitionend',function(){
+        $slides.hide()
+          .offset()
+        $slides.css({transform:'translateX(-1200px)'})
+          .show()
+      })
+  }else{
+    $slides.css({transform:'translateX(-1200px)'})
+  }
+  current = 2
+})
